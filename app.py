51bebb48bf0bb2ec4a4bfcb87cd351676c4ad904
gustavodasilvaarruda-droid <gustavodasilvaarruda@gmail.com>
2026-09-20 17:51:25 -0300

    Update print statement to reflect farewell message

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 app.secret_key = os.environ.get('FLASK_SECRET_KEY', 'chave_secreta_padrao_local')
 
 # AGORA SEGURO: Puxando direto das variáveis de ambiente que configuramos na tela
-SUPABASE_URL = os.environ.get('SUPABASE_URL')
+SUPABASE_URL = os.environ.get('https://fxldojcgvzdnmpoodqp.supabase.co')
 SUPABASE_KEY = os.environ.get('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ4bGRvamNndnpnZG5tcG9vZHFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk2MTI1MzYsImV4cCI6MjEwNTE4ODUzNn0.kHIj7TCWmMwyzdhWO01zrWwzhVfh9ZvbGlUFpRdMa7w')  # Use aqui a chave 'anon' (a segura) na sua tela
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
